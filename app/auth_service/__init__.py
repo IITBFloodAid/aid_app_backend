9757e4b80188d9c630807b34dc7a34cbe058cde7
Abhay <a3141b@gmail.com>
2025-11-01 20:46:39 +0530

    made some changes

--- app/auth_service/__init__.py
+++ app/auth_service/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-auth = Blueprint('auth', __name__, template_folder='templates')
+auth = Blueprint('auth', __name__)
 
 from app.auth_service import routes  # by this routes.py has been executed and routes are registered
