eb02cf95da8b4042a8840e9461cb4a99333276fa
Abhay <a3141b@gmail.com>
2025-11-01 20:57:16 +0530

    added the comments highlighting dev settings

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv() # kept for the dev purposes...
 mongo = PyMongo()
 
 def init_db(app):
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from app import create_app
 
 app = create_app()
 
+# below part is for the dev purposes...
 if __name__ == '__main__':
     app.run(debug=True)
